351bc18e8e0b97b4b9459099379b01c35c6b3a43
rakesh <thotarakesh28@gmail.com>
2023-11-19 04:58:21 +0000

    solution

---
+++ src/main/java/com/example/movie/model/Movie.java
@@ -0,0 +1,41 @@
+package com.example.movie.model;
+
+public class Movie {
+    private int movieId;
+    private String movieName;
+    private String leadActor;
+
+    // Constructors
+
+    public Movie(int movieId, String movieName, String leadActor) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.leadActor = leadActor;
+    }
+
+    // Getters and Setters
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public String getLeadActor() {
+        return leadActor;
+    }
+
+    public void setLeadActor(String leadActor) {
+        this.leadActor = leadActor;
+    }
+
+}
